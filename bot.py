440e564e060128843ce3819604b37e2c4ecd5724
EricNunesZ <eric2605@outlook.com>
2022-05-17 15:12:32 -0300

    No OS

--- bot.py
+++ bot.py
@@ -36,7 +36,7 @@ for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         bot.load_extension(f'cogs.{filename[:-3]}')
 
-os.chdir("./images")
+#os.chdir("./images")
 
 @bot.event
 async def on_ready():
