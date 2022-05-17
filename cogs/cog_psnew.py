98d399bb0bfdc93f15dc4dbc9b647121360d5a88
EricNunesZ <eric2605@outlook.com>
2022-05-17 15:09:45 -0300

    Ps try 2

--- cogs/cog_psnew.py
+++ cogs/cog_psnew.py
@@ -38,32 +38,5 @@ class cog_ps(commands.Cog):
         embed.set_thumbnail(url="https://i.imgur.com/A9X6IKH.gif")
         await ctx.reply(embed=embed)
 
-    @commands.command(name="mine")
-    @cooldown(1,5, type = commands.BucketType.user)
-    async def mine(self, ctx, *, mensagem=None):
-        userAvatar = ctx.author.avatar_url
-        url = requests.get(userAvatar)
-        if mensagem == None:
-            return await ctx.send(f"❌| {ctx.author.mention}, insira um texto.")
-        avatar = Image.open(BytesIO(url.content))
-        avatar = avatar.resize((80,80))
-        bigavatar = (avatar.size[0] * 3, avatar.size[1] * 3)
-        mascara = Image.new('L', bigavatar, 0)
-        recortar = ImageDraw.Draw(mascara)
-        recortar.ellipse((0, 0) + bigavatar, fill=255)
-        mascara = mascara.resize(avatar.size, Image.ANTIALIAS)
-        avatar.putalpha(mascara)
-        saida = ImageOps.fit(avatar, mascara.size, centering=(0.5, 0.5))
-        saida.putalpha(mascara)
-        saida.save('img_avatar.png')
-        img = Image.open("img_conquista.png")
-        fonte = ImageFont.truetype("font_Minecraft.ttf", 35)
-        escrever = ImageDraw.Draw(img)
-        escrever.text(xy=(118,75), text=f"{mensagem}", fill=(230, 230, 230), font=fonte)
-        img.paste(avatar, (25, 25), avatar)
-        img.save('img_conquistamine.png')
-        await ctx.send(content = f"**{ctx.author.mention} <:anicoin:919293624850727022>|**", file=discord.File('img_conquistamine.png'))
-
-    
 def setup(bot):
     bot.add_cog(cog_ps(bot))
