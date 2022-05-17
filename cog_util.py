fdcb03c7e8afc0e88edd9edf57617c7f98ba8928
EricNunesZ <eric2605@outlook.com>
2022-05-17 15:17:39 -0300

    No ps/util

--- cog_util.py
+++ cog_util.py
@@ -9,44 +9,11 @@ intents.members = True
 now = datetime.datetime.now()
 now = now.strftime("%d/%m/%Y - %H:%M:%S")
 
-def get_prefix(bot, message):
-    with open('prefixes.json', 'r') as f:
-        prefixes = json.load(f)
-    return prefixes[str(message.guild.id)]
-
-bot = commands.Bot(command_prefix = get_prefix, intents=intents,  case_insensitive = True)
+command_prefix = "a!"
+bot = commands.Bot(command_prefix = "a!", intents=intents,  case_insensitive = True)
 
 def cooldown(rate, per_sec=0, per_min=0, per_hour=0, type=commands.BucketType.default):
     return commands.cooldown(rate, per_sec + 60 * per_min + 3600 * per_hour, type)
-
-async def open_account(user):
-    users = await get_bank_data()
-
-    if str(user.id) in users:
-        return False
-    else:
-        users[str(user.id)] = {}
-        users[str(user.id)]["wallet"] = 0
-        users[str(user.id)]["bank"] = 0
-
-    with open("mainbank.json","w") as f:
-        json.dump(users, f)
-    return True
-
-async def get_bank_data():
-    with open("mainbank.json","r") as f:
-        users=json.load(f)
-
-    return users
-
-async def update_bank(user, change = 0, mode = "wallet"):
-    users = await get_bank_data()
-    users[str(user.id)][mode] += change 
-
-    with open("mainbank.json","w") as f:
-        json.dump(users, f)
-    bal = [users[str(user.id)]["wallet"],users[str(user.id)]["bank"]]
-    return bal
 
 current_language = "pt"
 
@@ -57,11 +24,8 @@ class cog_util(commands.Cog):
     @commands.command(name="util", aliases = ["🔑"])
     @cooldown(1,3, type = commands.BucketType.user)
     async def util(self, ctx):
-        with open('prefixes.json', 'r') as f:
-            prefixes = json.load(f)
-        prefix = prefixes[str(ctx.guild.id)]
         embed = discord.Embed(title = f"『🔑』Utilidades [22]",description =  "**`anime - avatar - banner - calc - channelinfo - color - data - emoji - emojiinfo - firstmessage - font - guildicon - languages - membros - roleinfo - roleuser - serverinfo - timer - translate - userinfo - userroles - weather`**",color = 0x308020)
-        embed.set_footer(text=f"• Para obter informações de cada comando, digite {prefix}help <comando>", icon_url=self.bot.user.avatar_url)
+        embed.set_footer(text=f"• Para obter informações de cada comando, digite {command_prefix}help <comando>", icon_url=self.bot.user.avatar_url)
         embed.set_thumbnail(url="https://i.imgur.com/heRA5YW.gif")
         await ctx.reply(embed=embed)
 
@@ -116,19 +80,13 @@ class cog_util(commands.Cog):
         now = now.strftime("%d/%m/%Y - %H:%M:%S")
         if not member:
             member = ctx.message.author
-        await open_account(ctx.author)
-        users = await get_bank_data()
-        earnings = 3
-        users[str(ctx.author.id)]["wallet"] += earnings
-        with open("mainbank.json","w") as f:
-            json.dump(users,f)
         userAvatar = member.avatar_url
         embed = discord.Embed(
             title = "🖼 " + ctx.author.name,
             description = f"Clique [aqui]({userAvatar}) para baixar",
             color = 0x308020
         )
-        embed.set_footer(text="Pedido por " + ctx.author.name + " em " + now + f"| 💰 +{earnings}", icon_url=ctx.author.avatar_url)
+        embed.set_footer(text="Pedido por " + ctx.author.name + " em " + now + f"| 💰", icon_url=ctx.author.avatar_url)
         embed.set_image(url=userAvatar)
         await ctx.send(embed=embed)
 
@@ -143,12 +101,6 @@ class cog_util(commands.Cog):
             return
         expression = "".join(expression)
         response = eval(expression)
-        await open_account(ctx.author)
-        users = await get_bank_data()
-        earnings = 2
-        users[str(ctx.author.id)]["wallet"] += earnings
-        with open("mainbank.json","w") as f:
-            json.dump(users,f)
         await ctx.send("A resposta é: " + str(response))
 
     @commands.command(name="channelinfo", aliases = ["infochannel","chatinfo"])
@@ -156,13 +108,6 @@ class cog_util(commands.Cog):
     async def channelinfo(self, ctx):
         member = ctx.message.author
         nsfw = "Sim" if ctx.channel.is_nsfw() else "Não"
-        await open_account(ctx.author)
-        users = await get_bank_data()
-        user = ctx.author
-        earnings = 1
-        users[str(user.id)]["wallet"] += earnings
-        with open("mainbank.json","w") as f:
-            json.dump(users,f)
         embed = discord.Embed(
             title = f"Informações do canal:",
             color = 0x308020
@@ -178,7 +123,7 @@ class cog_util(commands.Cog):
 
         for name, value, inline in fields:
             embed.add_field(name=name, value=value, inline=inline)
-        embed.set_footer(text="Requisitado por " + ctx.author.name + " às " + now + f"| 💰 +{earnings}", icon_url=member.avatar_url)
+        embed.set_footer(text="Requisitado por " + ctx.author.name + " às " + now + f"| 💰", icon_url=member.avatar_url)
 
         await ctx.send(embed=embed)
 
@@ -187,14 +132,7 @@ class cog_util(commands.Cog):
     async def data(self, ctx):
         now = datetime.datetime.now()
         now = now.strftime("%d/%m/%Y - %H:%M:%S")
-        await open_account(ctx.author)
-        users = await get_bank_data()
-        user = ctx.author
-        earnings = 2
-        users[str(user.id)]["wallet"] += earnings
-        with open("mainbank.json","w") as f:
-            json.dump(users,f)
-        await ctx.reply(f"⌚| Data e hora atual: **{now}**\n<:anicoin:919293624850727022>| +{earnings}")
+        await ctx.reply(f"⌚| Data e hora atual: **{now}**\n<:anicoin:919293624850727022>|")
 
     @commands.command(name="emoji", aliases=["emj"])
     @cooldown(1,3, type = commands.BucketType.user)
@@ -202,12 +140,6 @@ class cog_util(commands.Cog):
         if not emoji:
             await ctx.send(f"❌| {ctx.author.mention}, informe o emoji.")
         else:
-            await open_account(ctx.author)
-            users = await get_bank_data()
-            earnings = 1
-            users[str(ctx.author.id)]["wallet"] += earnings
-            with open("mainbank.json","w") as f:
-                json.dump(users,f)
             await ctx.send(emoji.url)
 
     @commands.command(name="emojiinfo", aliases = ["emojinfo","infoemoji"])
@@ -227,19 +159,13 @@ class cog_util(commands.Cog):
         emjCreation = emoji.created_at.strftime("%d/%m/%Y às %H:%M:%S")
         emojiUse = "@everyone" if not emoji.roles else " ".join(role.name for role in emoji.roles)
 
-        await open_account(ctx.author)
-        users = await get_bank_data()
-        earnings = 1
-        users[str(ctx.author.id)]["wallet"] += earnings
-        with open("mainbank.json","w") as f:
-            json.dump(users,f)
         embed = discord.Embed(
             title = f"Informações do emoji: <:{emoji.name}:{emoji.id}>",
             description = f"**✏| Nome:** `{emoji.name}`\n**ℹ| ID:** `{emoji.id}`\n**@| Código:** `{emoji}`\n**⏬| Download:** [Link para baixar]({emoji.url})\n**👑| Criador:** {emoji.user.mention}\n**📆| Criado em:** `{emjCreation}`\n**👥| Quem pode usar:** {emojiUse}\n**🌠| Animado:** `{emjAnimated}`\n**⚙| Restrito à moderação:** `{emjManaged}`\n**🌆| Servidor do emoji:** `{emoji.guild.name}`",
             color = 0x308020
         )
         embed.set_thumbnail(url=emoji.url)
-        embed.set_footer(text=f"Pedido por {ctx.author.name} em {now} | 💰 +{earnings}", icon_url=ctx.author.avatar_url)
+        embed.set_footer(text=f"Pedido por {ctx.author.name} em {now} | 💰", icon_url=ctx.author.avatar_url)
         await ctx.send(embed=embed)
 
     @commands.command(name="userinfo", aliases = ["ui","infouser","memberinfo","perfil"])
@@ -253,12 +179,6 @@ class cog_util(commands.Cog):
         userAvatar = member.avatar_url
         creation_time = member.created_at.strftime("%d/%m/%Y às %H:%M:%S")
         joined_time = member.joined_at.strftime("%d/%m/%Y às %H:%M:%S")
-        await open_account(ctx.author)
-        users = await get_bank_data()
-        earnings = 1
-        users[str(ctx.author.id)]["wallet"] += earnings
-        with open("mainbank.json","w") as f:
-            json.dump(users,f)
         embed = discord.Embed(
             title = f"{member.name}",
             color = member.colour
@@ -273,7 +193,7 @@ class cog_util(commands.Cog):
         embed.add_field(name="🗓| Conta criada em:", value=f"`{creation_time}`", inline=True)
         embed.add_field(name="📆| Entrou em:", value=f"`{joined_time}`", inline=True)
         embed.set_thumbnail(url=userAvatar)
-        embed.set_footer(text="Pedido por " + ctx.author.name + " em " + now + f"| 💰 +{earnings}", icon_url=ctx.author.avatar_url)
+        embed.set_footer(text="Pedido por " + ctx.author.name + " em " + now + f"| 💰", icon_url=ctx.author.avatar_url)
         await ctx.send(embed=embed)
 
     @commands.command(name="roleinfo", aliases = ["ri","inforole"])
@@ -292,12 +212,6 @@ class cog_util(commands.Cog):
             if role in user.roles:
                 membros.append(f"『{user.mention}』")
         number_users = len(membros)
-        await open_account(ctx.author)
-        users = await get_bank_data()
-        earnings = 1
-        users[str(ctx.author.id)]["wallet"] += earnings
-        with open("mainbank.json","w") as f:
-            json.dump(users,f)
         embed = discord.Embed(
             title = f"{role.name}",
             color = role.colour
@@ -312,7 +226,7 @@ class cog_util(commands.Cog):
         embed.add_field(name="🤖| Integração:", value=f"`{bot_managed}`", inline=True)
         #embed.add_field(name="🔧| Gerenciado por bot:", value=f"`{bot_managed}`", inline=True)
         embed.set_thumbnail(url=ctx.guild.icon_url)
-        embed.set_footer(text="Requisitado por " + ctx.author.name + " às " + now + f"| 💰 +{earnings}", icon_url=ctx.author.avatar_url)
+        embed.set_footer(text="Requisitado por " + ctx.author.name + " às " + now + f"| 💰", icon_url=ctx.author.avatar_url)
         await ctx.send(embed=embed)
 
     @commands.command(name="roleuser", aliases=["userrole","userole"])
@@ -321,19 +235,13 @@ class cog_util(commands.Cog):
         membros = []
         role = discord.utils.find(
             lambda r: r.id == role.id, ctx.guild.roles)
-        await open_account(ctx.author)
-        users = await get_bank_data()
-        earnings = 2
-        users[str(ctx.author.id)]["wallet"] += earnings
-        with open("mainbank.json","w") as f:
-            json.dump(users,f)
         for user in ctx.guild.members:
             if role in user.roles:
                 membros.append(f"『{user.mention}』")
         number_users = len(membros)
         embed = discord.Embed(title = f"🧳| Usuários com a role {role.name} 『{number_users}』", description = "".join(reversed(membros)), color = 0x308020)
         embed.set_thumbnail(url=ctx.guild.icon_url)
-        embed.set_footer(text="Requisitado por " + ctx.author.name + " em " + now + f"| 💰 +{earnings}", icon_url=ctx.author.avatar_url)
+        embed.set_footer(text="Requisitado por " + ctx.author.name + " em " + now + f"| 💰", icon_url=ctx.author.avatar_url)
         await ctx.send(embed = embed)
 
     @commands.command(name="serverinfo", aliases = ["infoserver","guildinfo","si"])
@@ -350,13 +258,6 @@ class cog_util(commands.Cog):
         roles = len(ctx.guild.roles)
         invites = len(await ctx.guild.invites())
         emojis = len(ctx.guild.emojis)
-        await open_account(ctx.author)
-        users = await get_bank_data()
-        user = ctx.author
-        earnings = 1
-        users[str(user.id)]["wallet"] += earnings
-        with open("mainbank.json","w") as f:
-            json.dump(users,f)
         embed = discord.Embed(
             title = f"Informações do Servidor:",
             color = 0x308020
@@ -394,37 +295,25 @@ class cog_util(commands.Cog):
     @cooldown(1,3, type = commands.BucketType.user)
     async def servers(self, ctx):
         servidores = []
-        await open_account(ctx.author)
-        users = await get_bank_data()
-        earnings = 2
-        users[str(ctx.author.id)]["wallet"] += earnings
-        with open("mainbank.json","w") as f:
-            json.dump(users,f)
         for guild in bot.guilds:
             servidores.append(f"『{guild.name}』")
             print(guild.name)
         number_guilds = len(servidores)
         embed = discord.Embed(title = f"🌎| Servidores onde eu estou: 『{number_guilds}』", description = "".join(servidores), color = 0x308020)
         embed.set_thumbnail(url=ctx.guild.icon_url)
-        embed.set_footer(text="Requisitado por " + ctx.author.name + " em " + now + f"| 💰 +{earnings}", icon_url=ctx.author.avatar_url)
+        embed.set_footer(text="Requisitado por " + ctx.author.name + " em " + now + f"| 💰", icon_url=ctx.author.avatar_url)
         await ctx.send(embed = embed)
 
     @commands.command(name="userroles", aliases=["roles","useroles"])
     @cooldown(1,3, type = commands.BucketType.user)
     async def userroles(self, ctx, member: discord.Member):
         roles = []
-        await open_account(ctx.author)
-        users = await get_bank_data()
-        earnings = 1
-        users[str(ctx.author.id)]["wallet"] += earnings
-        with open("mainbank.json","w") as f:
-            json.dump(users,f)
         for role in member.roles:
             if role.name != "@everyone":
                 roles.append(f"『<@&{role.id}>』")
         number_roles = len(roles)
         embed = discord.Embed(title = f"💼| Cargos do @{member} 『{number_roles}』", description = "".join(reversed(roles)), color = 0x308020)
-        embed.set_footer(text="Requisitado por " + ctx.author.name + " às " + now + f"| 💰 +{earnings}", icon_url=ctx.author.avatar_url)
+        embed.set_footer(text="Requisitado por " + ctx.author.name + " às " + now + f"| 💰", icon_url=ctx.author.avatar_url)
         await ctx.send(embed = embed)
 
 def setup(bot):
