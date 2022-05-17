fb5f7a5370d789fe40656dc064d0c0d6f57d7666
EricNunesZ <eric2605@outlook.com>
2022-05-17 15:57:30 -0300

    Only cogs

--- bot.py
+++ bot.py
@@ -23,6 +23,20 @@ bot = commands.Bot(command_prefix = "a!", case_insensitive = True,  intents = in
 command_prefix = "a!"
 bot.remove_command("help")
 
+@bot.command()
+async def load(ctx, extension):
+    bot.load_extension(f"cogs.{extension}")
+
+@bot.command()
+async def unload(ctx, extension):
+    bot.unload_extension(f"cogs.{extension}")
+
+for filename in os.listdir('./cogs'):
+    if filename.endswith('.py'):
+        bot.load_extension(f'cogs.{filename[:-3]}')
+
+#os.chdir("./images")
+
 
 
 dotenv.load_dotenv()
